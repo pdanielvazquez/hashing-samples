f21603c1d92bc0f898f16e5e5803dea6be380499
DavidBo9 <194228@iberopuebla.mx>
2026-03-17 13:00:42 -0600

    Refactor insertion method in tablaHash class to improve clarity and maintainability

--- tablaHash.cpp
+++ tablaHash.cpp
@@ -12,12 +12,16 @@ class tablaHash{
 			return clave % size;
 		}
 	public:
+        tablaHash(int s): size(s), table(s){} //Constructor para inicializar el tamaño y la tabla hash
     //Agregamos el metodo de insercion
         void insertar(int clave){ //Recibimos nuestra clave a insertar
             int indice = hashModulo(clave); //Calculamos el indice con la funcion hash
-            table[indice].push_back(clave); // Insertar en la lista en el indice calculado
-        }
+            table[indice].push_back(clave);
+            //imprimir el indice calculado y la clave insertada
+            cout<<"Clave "<<clave<<" insertada en el indice "<<indice<<endl;
 
+        }
+        //metodo imprimir
         void imprimir(){
             for (int i = 0; i < size; i++)
             {
@@ -33,7 +37,6 @@ class tablaHash{
             
         }
 
-
 };
 
 
@@ -46,10 +49,9 @@ int main(){
     hash.insertar(15);
     hash.insertar(20);
     hash.insertar(32);
-    hash.insertar(9);
+    hash.insertar(7);
 
     hash.imprimir();
    
 
 }
-
